59a50e4c413edf92fbea548cf484b8573f9fa691
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-10-13 09:36:43 +0200

    refactor: updated orders UI

--- app/profile/orders/page.tsx
+++ app/profile/orders/page.tsx
@@ -14,7 +14,7 @@ export default async function OrdersPage({
   return (
     <main className="h-screen bg-gray-50 md:pt-[74px] md:pb-[92px] flex justify-center overflow-y-hidden">
       <section className="w-full max-w-4xl md:bg-white md:rounded-xl md:shadow-md md:p-6">
-        <div className="flex flex-row gap-3 items-center mb-8 p-4">
+        <div className="flex flex-row gap-3 items-center mb-2 md:mb-8 p-4">
           <BackButton url={redirect || "/profile"} />
           <h1 className="text-2xl font-bold text-lunar-green-800">My Orders</h1>
         </div>
